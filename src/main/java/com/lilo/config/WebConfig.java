6e086f4016870ab278d59ec3c155675f606cdee7
Mohammed Lilo <mohammedlilosoci@gmail.com>
2026-02-19 18:39:39 +0300

    Add Logging using Logbook library

--- src/main/java/com/lilo/config/WebConfig.java
+++ src/main/java/com/lilo/config/WebConfig.java
@@ -2,9 +2,24 @@ package com.lilo.config;
 
 import com.lilo.service.FileStorageService;
 import com.lilo.shared.WebConstants;
+import org.apache.tika.mime.MimeTypes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.util.MimeType;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.zalando.logbook.BodyFilter;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.BodyFilters;
+
+import java.util.Collections;
+
+import static java.util.Collections.singleton;
+import static org.zalando.logbook.core.BodyFilters.defaultValue;
+import static org.zalando.logbook.core.Conditions.contentType;
+import static org.zalando.logbook.core.Conditions.exclude;
+import static org.zalando.logbook.json.JsonBodyFilters.replaceJsonStringProperty;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -24,4 +39,18 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations(FileStorageService.ROOT.toUri().toString())
                 .setCachePeriod(3600);
     }
+//    @Bean
+//    public BodyFilter bodyFilter() {
+//        return BodyFilter.merge(
+//                defaultValue(),
+//                replaceJsonStringProperty(singleton("secret"), "XXX"));
+//    }
+//@Bean
+//public Logbook logbook() {
+//    return Logbook.builder()
+//            .condition(exclude(
+//                    contentType(MimeTypes.OCTET_STREAM, "multipart/form-data", "image/*", "video/*")
+//            ))
+//            .build();
+//}
 }
